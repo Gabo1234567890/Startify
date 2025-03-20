e47023d6e23128cf9de68567216f6c1ec3e1e70f
Gabo1234567890 <gabonenov@gmail.com>
2025-03-21 00:15:20 +0200

    nav bar logic fixed

--- frontend/startify/lib/widgets/nav_bar_widget.dart
+++ frontend/startify/lib/widgets/nav_bar_widget.dart
@@ -13,52 +13,57 @@ class NavBarWidget extends StatelessWidget {
         if (selectedPage != 0) {
           whereIsHome[selectedPage - 1] = true;
         }
-        return NavigationBar(
-          destinations: [
-            NavigationDestination(
-              icon:
-                  whereIsHome[0]
-                      ? Icon(Icons.home_outlined)
-                      : Icon(Icons.chat_outlined),
-              label: '',
-            ),
-            NavigationDestination(
-              icon:
-                  whereIsHome[1]
-                      ? Icon(Icons.home_outlined)
-                      : Icon(Icons.attach_money),
-              label: '',
-            ),
-            NavigationDestination(
-              icon:
-                  whereIsHome[2]
-                      ? Icon(Icons.home_outlined)
-                      : Icon(Icons.calendar_month_outlined),
-              label: '',
-            ),
-            NavigationDestination(
-              icon:
-                  whereIsHome[3]
-                      ? Icon(Icons.home_outlined)
-                      : Icon(Icons.lightbulb_outline),
-              label: '',
-            ),
-            NavigationDestination(
-              icon:
-                  whereIsHome[4]
-                      ? Icon(Icons.home_outlined)
-                      : Icon(Icons.person_outline),
-              label: '',
-            ),
-          ],
-          onDestinationSelected: (int value) {
-            if (selectedPage - 1 == value) {
-              selectedPageNotifier.value = 0;
-            } else {
-              selectedPageNotifier.value = value + 1;
-            }
-          },
-          selectedIndex: selectedPage,
+        return NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: selectedPage == 0 ? Colors.transparent : null,
+          ),
+          child: NavigationBar(
+            destinations: [
+              NavigationDestination(
+                icon:
+                    whereIsHome[0]
+                        ? Icon(Icons.home_outlined)
+                        : Icon(Icons.chat_outlined),
+                label: '',
+              ),
+              NavigationDestination(
+                icon:
+                    whereIsHome[1]
+                        ? Icon(Icons.home_outlined)
+                        : Icon(Icons.attach_money),
+                label: '',
+              ),
+              NavigationDestination(
+                icon:
+                    whereIsHome[2]
+                        ? Icon(Icons.home_outlined)
+                        : Icon(Icons.calendar_month_outlined),
+                label: '',
+              ),
+              NavigationDestination(
+                icon:
+                    whereIsHome[3]
+                        ? Icon(Icons.home_outlined)
+                        : Icon(Icons.lightbulb_outline),
+                label: '',
+              ),
+              NavigationDestination(
+                icon:
+                    whereIsHome[4]
+                        ? Icon(Icons.home_outlined)
+                        : Icon(Icons.person_outline),
+                label: '',
+              ),
+            ],
+            onDestinationSelected: (int value) {
+              if (selectedPage - 1 == value) {
+                selectedPageNotifier.value = 0;
+              } else {
+                selectedPageNotifier.value = value + 1;
+              }
+            },
+            selectedIndex: selectedPage == 0 ? selectedPage : selectedPage - 1,
+          ),
         );
       },
     );
