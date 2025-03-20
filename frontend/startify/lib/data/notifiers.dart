7f1d9c6a9bdd4dc21902ab5a1ca614180777d596
Gabo1234567890 <gabonenov@gmail.com>
2025-03-20 16:54:40 +0200

    complex search bar made

--- frontend/startify/lib/data/notifiers.dart
+++ frontend/startify/lib/data/notifiers.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 ValueNotifier selectedPageNotifier = ValueNotifier(0);
 ValueNotifier darkModeNotifier = ValueNotifier(false);
+ValueNotifier ideaSearchNotifier = ValueNotifier(true);
