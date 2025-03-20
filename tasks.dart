0b84f3ba27e0cd2b2cb81994f24f6c8edbfd8474
Gabo1234567890 <gabonenov@gmail.com>
2025-03-20 13:08:59 +0200

    tasks set

---
+++ tasks.dart
@@ -0,0 +1,12 @@
+//* Frontend Tasks
+// TODO Home page
+// TODO Profile page
+// TODO Login page
+// TODO Schedule page
+// TODO Idea page
+// TODO Chat page
+// TODO RestAPI
+
+//* Backend Tasks
+// TODO Database
+// TODO
