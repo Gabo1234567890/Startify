f259af1b6af7ed83c07ba7d413e8bd61aed58c60
HyperRaptor <moni.aleksiev07@gmail.com>
2025-03-20 18:38:35 +0200

    Fixed login being part of main

--- backend/startify/urls.py
+++ backend/startify/urls.py
@@ -16,12 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import CustomTokenObtainPairView, register_user
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', register_user, name='register'),
 ]
