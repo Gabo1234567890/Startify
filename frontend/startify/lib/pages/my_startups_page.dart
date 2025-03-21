5b51df61016e33114884e4365bb6af650a61c0ec
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-03-21 10:52:25 +0200

    my startup page

--- frontend/startify/lib/pages/my_startups_page.dart
+++ frontend/startify/lib/pages/my_startups_page.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:startify/widgets/idea_card_widget.dart';
 
 class MyStartupsPage extends StatelessWidget {
   const MyStartupsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: ClampingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: Text(
+                  'Startups: 4',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 20.0, top: 5.0),
+                    child: SizedBox(
+                      height: 45,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          hintText: 'Search Startups...',
+                          contentPadding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ListView(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  IdeaCardWidget(),
+                  IdeaCardWidget(),
+                  IdeaCardWidget(),
+                  IdeaCardWidget(),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
