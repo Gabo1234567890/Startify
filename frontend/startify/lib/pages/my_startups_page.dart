b12057c4ad2b25b5395451d150661a81acb9adeb
Gabo1234567890 <gabonenov@gmail.com>
2025-03-21 09:59:26 +0200

    investments done and my startups created

---
+++ frontend/startify/lib/pages/my_startups_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyStartupsPage extends StatelessWidget {
+  const MyStartupsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- frontend/startify/lib/widget_tree.dart
+++ frontend/startify/lib/widget_tree.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:startify/data/notifiers.dart';
 import 'package:startify/pages/home_page.dart';
 import 'package:startify/pages/investments_page.dart';
+import 'package:startify/pages/my_startups_page.dart';
 import 'package:startify/pages/profile_page.dart';
 import 'package:startify/widgets/app_bar_widget.dart';
 import 'package:startify/widgets/nav_bar_widget.dart';
 
-List<Widget> pages = [HomePage(), InvestmentsPage(), ProfilePage()];
+List<Widget> pages = [
+  HomePage(),
+  InvestmentsPage(),
+  MyStartupsPage(),
+  ProfilePage(),
+];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
