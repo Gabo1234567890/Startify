2fe9d8a138800461528e7c669941bd75c1378be4
Gabo1234567890 <gabonenov@gmail.com>
2025-03-22 01:37:15 +0200

    create account page made

---
+++ frontend/startify/lib/pages/create_account_page.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:startify/pages/home_page.dart';
+import 'package:startify/pages/login_page.dart';
+import 'package:startify/services/auth_service.dart';
+import 'package:startify/widgets/app_bar_widget_nologin.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class CreateAccountPage extends StatefulWidget {
+  const CreateAccountPage({super.key});
+
+  @override
+  State<CreateAccountPage> createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  bool _isPasswordVisible = false;
+  bool _isLoading = false;
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  void _handleRegister() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final response = await AuthService().register(
+        _emailController.text,
+        _passwordController.text,
+        _usernameController.text,
+      );
+
+      final dynamic token = response['token'];
+      await _storage.write(key: 'auth_token', value: token);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Welcome, ${response['user']['username']}!")),
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Account creation failed: ${e.toString()}")),
+      );
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidgetNoLogIn(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+              const Icon(Icons.person_outline, size: 200),
+              const SizedBox(height: 40),
+
+              // Username Field
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              // Email Field
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              // Password Field
+              TextField(
+                controller: _passwordController,
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              ElevatedButton(
+                onPressed: _isLoading ? null : _handleRegister,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                child:
+                    _isLoading
+                        ? CircularProgressIndicator(color: Colors.white)
+                        : const Text(
+                          "Create Account",
+                          style: TextStyle(fontSize: 18),
+                        ),
+              ),
+
+              const SizedBox(height: 15),
+
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                  //! Handle create account logic
+                },
+                child: const Text(
+                  "Already have an account? Sign in here!",
+                  style: TextStyle(
+                    fontSize: 12,
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 2,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
