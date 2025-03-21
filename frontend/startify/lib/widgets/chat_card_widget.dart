15cb0f909c2b12ba20e16daa462fb78f206faeca
Gabo1234567890 <gabonenov@gmail.com>
2025-03-21 12:48:09 +0200

    chat page made

---
+++ frontend/startify/lib/widgets/chat_card_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ChatCardWidget extends StatelessWidget {
+  const ChatCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 0),
+      child: Column(
+        children: [
+          ListTile(
+            minVerticalPadding: 25,
+            onTap: () {},
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('lib/assets/avatar2.png'),
+              radius: 35,
+            ),
+            title: Text('Name', style: TextStyle(fontWeight: FontWeight.bold)),
+            subtitle: Text(
+              'Last message • 19:03',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  'Specialization',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Divider(height: 0, thickness: 3),
+        ],
+      ),
+    );
+  }
+}
