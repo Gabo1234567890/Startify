4208556c96d0673932bc8d30af77d4839687d0e0
Gabo1234567890 <gabonenov@gmail.com>
2025-04-20 17:26:11 +0300

    New tasks

--- tasks.dart
+++ tasks.dart
@@ -1,19 +1,13 @@
-//* Frontend Tasks
-// Home page 
-// Profile page
-// Login page
-// Schedule page
-// My startups page
-// TODO My startup page
-// TODO My investments page
-// TODO My invested startup page
-// TODO Chat page
-// TODO RestAPI
+//TODO Person Card working
+//TODO Person profile info from database
+//TODO Investments money from profile database
+//TODO Chat
+//TODO Search
+//TODO Catalog-like
 
-//* Backend Tasks
-// TODO User model
-// TODO Login model
-// TODO Chat model
-// TODO Message model
-// TODO Event model
-// TODO Connect frontend buttons
+//* Dependent on create idea page
+//TODO My idea get info from database
+//TODO Other idea get info from database
+
+//! //TODO Research legal documents
+//TODO Contracts
