ed8bd178be5595cf85bbd2491b2dfb378ebca4a6
Gabo1234567890 <gabonenov@gmail.com>
2025-04-20 15:17:01 +0300

    log out option made

--- frontend/startify/lib/widgets/app_bar_widget.dart
+++ frontend/startify/lib/widgets/app_bar_widget.dart
@@ -1,23 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:startify/data/notifiers.dart';
 import 'package:startify/pages/login_page.dart';
 
-class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+class AppBarWidget extends StatefulWidget implements PreferredSizeWidget {
   const AppBarWidget({super.key});
+
+  @override
+  State<AppBarWidget> createState() => _AppBarWidgetState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _AppBarWidgetState extends State<AppBarWidget> {
+  final storage = FlutterSecureStorage();
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
+  Future<void> checkLoginStatus() async {
+    final token = await storage.read(key: 'access_token');
+    setState(() {
+      isLoggedIn = token != null;
+    });
+  }
+
+  Future<void> logout() async {
+    await storage.delete(key: 'access_token');
+    setState(() {
+      isLoggedIn = false;
+    });
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       leading: Padding(
-        padding: const EdgeInsets.only(left: 15.0),
+        padding: const EdgeInsets.only(left: 5.0),
         child: GestureDetector(
           onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LoginPage()),
-            );
+            isLoggedIn
+                ? logout()
+                : Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
           },
-          child: Center(child: Text('Log In'))
+          child: Center(child: Text(isLoggedIn ? 'Log Out' : 'Log In')),
         ),
       ),
       title: Text('Startify', style: TextStyle(fontWeight: FontWeight.bold)),
@@ -37,7 +75,4 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
       ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
