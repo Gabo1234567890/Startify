b0f683329f12affc10e9d722308c919b1f6d61cb
Gabo1234567890 <gabonenov@gmail.com>
2025-04-20 18:39:48 +0300

    updated tasks

--- tasks.dart
+++ tasks.dart
@@ -1,13 +1,15 @@
-//TODO Person Card working
-//TODO Person profile info from database
 //TODO Investments money from profile database
 //TODO Chat
 //TODO Search
 //TODO Catalog-like
+
+//* Dependent on other's profile page
+//TODO Person Card working
+//TODO Person profile info from database
 
 //* Dependent on create idea page
 //TODO My idea get info from database
 //TODO Other idea get info from database
 
 //! //TODO Research legal documents
-//TODO Contracts
+//TODO Contracts and Documents
