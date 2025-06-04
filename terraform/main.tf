795df3c3d87afed6db9be4c1d73c823b3d3a3d86
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-06-04 23:37:56 +0300

    terraform and docker

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Create a Docker network
+resource "docker_network" "app_network" {
+  name = "startify_network"
+}
+
+# Create a directory for PostgreSQL data
+resource "null_resource" "create_db_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.cwd}/postgres-data"
+  }
+}
+
+# PostgreSQL container
+resource "docker_image" "postgres" {
+  name         = "postgres:14"
+  keep_locally = true
+}
+
+resource "docker_container" "postgres" {
+  name  = "startify_db"
+  image = docker_image.postgres.image_id
+  env = [
+    "POSTGRES_USER=startify_user",
+    "POSTGRES_PASSWORD=startify_pass",
+    "POSTGRES_DB=startify_db"
+  ]
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+  ports {
+    internal = 5432
+    external = 5433
+  }
+  volumes {
+    container_path = "/var/lib/postgresql/data"
+    host_path      = "${abspath(path.cwd)}/postgres-data" # Absolute path
+  }
+  depends_on = [null_resource.create_db_dir]
+}
+
+# FastAPI container
+resource "docker_image" "app" {
+  name = "startify-app"
+  build {
+    context = abspath("${path.module}/../backend")
+    dockerfile = "Dockerfile"
+  }
+}
+
+resource "docker_container" "app" {
+  name  = "startify_api"
+  image = docker_image.app.image_id
+  env = [
+    "DATABASE_URL=postgresql://startify_user:startify_pass@startify_db:5432/startify_db"
+  ]
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+  ports {
+    internal = 8000
+    external = 8000
+  }
+  depends_on = [docker_container.postgres]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "Name of the project"
+  default     = "startify"
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "db_username" {
+  description = "Database username"
+  default     = "startify_user"
+}
+
+variable "db_password" {
+  description = "Database password"
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "Database name"
+  default     = "startify_db"
+}
